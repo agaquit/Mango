7f0f97815a632f5dc840aaa7042dc2c9d3c5812f
ANTONIOGAQUIT\anton <antonia.gaquit@wiccon.se>
2024-02-07 14:23:37 +0100

    Add project files.

---
+++ Mango.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+using System.Net.NetworkInformation;
+
+namespace Mango.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CouponDto, Coupon>();
+                config.CreateMap<Coupon, CouponDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
